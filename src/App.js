af7514df22b5a837e1dc62844470d9427536529f
kittybagpipes <sparticus978@gmail.com>
2020-11-09 14:03:45 -0800

    wireframed, adding functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return <main className="App">{/* content goes here */}</main>;
+import AddBookmark from "./AddBookmark/AddBookmark";
+import BookmarkApp from "./BookmarkApp/BookmarkApp";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false,
+    };
+  }
+  render() {
+    const page = this.state.showAddForm ? (
+      <AddBookmark />
+    ) : (
+      <BookmarkApp bookmarks={this.state.bookmarks} />
+    );
+    return <div className="App">{page}</div>;
+  }
 }
 
 export default App;
